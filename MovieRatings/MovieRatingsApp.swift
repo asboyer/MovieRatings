468f9d12ec781a9bcd7e31e72e25a79a52eea1c9
Andrew Boyer <asboyer@gmail.com>
2023-09-21 19:31:40 -0400

    first commit

---
+++ MovieRatings/MovieRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRatingsApp.swift
+//  MovieRatings
+//
+//  Created by baller on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
